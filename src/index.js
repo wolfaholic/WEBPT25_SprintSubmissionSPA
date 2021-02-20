aeb4cc89ac44ba8f6d6de712f83f56d29ecfdc8f
Melissa <wolfaholic@users.noreply.github.com>
2021-02-19 18:54:36 -0800

    Answered Questions
    Set Up Branch, answered questions set up imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import {BrowserRouter as Router} from "react-router-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById('root')
+ReactDOM.render(
+<Router>
+<App />
+</Router>, rootElement
+);
